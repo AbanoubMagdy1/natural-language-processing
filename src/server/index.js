eb4f1d2c503fdb1fcd7f936cfac50be8eb6a2075
AbanoubMagdy1 <bebomagdy666@gmail.com>
2021-04-06 14:12:48 +0200

    Initialize app and configure webpack

---
+++ src/server/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static('dist'));
+
+console.log(__dirname);
+
+app.get('/', function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve('src/client/views/index.html'));
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get('/test', function (req, res) {
+  res.send(mockAPIResponse);
+});
